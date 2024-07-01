1a903e3b218ceaeb36f95eb92afdf71933955819
Rakshit Dubey <119107044+RakshitDubey@users.noreply.github.com>
2024-07-01 02:39:31 -0700

    add a function sum

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Rakshit Dubey");
+
+
+function add(arr){
+
+    let sum=0
+    for (let index = 0; index < arr.length; index++) {
+        sum+=arr[index]
+
+        
+
+    }
+    return sum
+
+}
+
